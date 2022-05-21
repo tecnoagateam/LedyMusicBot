75cb131ed9e3e1c1b63e13760415f0e29592d897
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-05-21 12:30:49 +0400

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -27,7 +27,7 @@ def bul(client, message):
 
     query = "".join(" " + str(i) for i in message.command[1:])
     print(query)
-    m = message.reply("🔎 tapıram...")
+    m = message.reply("🔎 Tapıram...")
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=5).to_dict()
@@ -55,7 +55,7 @@ def bul(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"☑️ **Ad**: [{title[:35]}]({link})\n🎬 **Cloud**: YouTube\n⏱️ **Vaxt**: `{duration}`\n👁‍🗨 **İzlənmə**: `{views}`\n📤 **Tərəfindən**: @SSmusicLedy_bot"
+        rep = f"☑️ **Ad**: [{title[:35]}]({link})\n🎬 **servis**: YouTube\n⏱️ **Vaxt**: `{duration}`\n👁‍🗨 **İzlənmə**: `{views}`\n📤 **Tərəfindən**: @SSmusicLedy_bot"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
@@ -107,14 +107,14 @@ async def vsong(client, message):
     except Exception as e:
         print(e)
     try:
-        msg = await message.reply("📥 **video endirirəm...**")
+        msg = await message.reply("📥 **videonu yukləyirəm...**")
         with YoutubeDL(ydl_opts) as ytdl:
             ytdl_data = ytdl.extract_info(link, download=True)
             file_name = ytdl.prepare_filename(ytdl_data)
     except Exception as e:
         return await msg.edit(f"🚫 **Xəta:** {e}")
     preview = wget.download(thumbnail)
-    await msg.edit("📤 **video göndərirəm...**")
+    await msg.edit("📤 **videonu göndərirəm...**")
     await message.reply_video(
         file_name,
         duration=int(ytdl_data["duration"]),
