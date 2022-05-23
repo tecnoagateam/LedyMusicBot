b098037acb5ebeb1d1ba630007dd2bb1343efa61
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-05-23 20:25:43 +0400

    Update config.py

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "TaliaMüzik") 
+BOT_NAME = getenv("BOT_NAME", "Ledy Music") 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "Efsanestar_bot")
+BOT_USERNAME = getenv("BOT_USERNAME", "SSmusicLedy_bot")
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "500"))
 
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! . $").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
