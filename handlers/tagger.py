c7942ab75ce4e51020c3c57d66379ea6058f5414
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-05-28 21:11:55 +0400

    Create tagger.py

---
+++ handlers/tagger.py
@@ -0,0 +1,282 @@
+import random
+import os, logging, asyncio
+from telethon import Button
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+from telethon.tl.types import ChannelParticipantsAdmins
+from telethon.events import StopPropagation
+
+
+anlik_calisan = []
+elxan_tag = []
+#tektag
+@client.on(events.NewMessage(pattern='^(?i)/cancel'))
+async def cancel(event):
+  global elxan_tag
+  elxan_tag.remove(event.chat_id)
+
+@client.on(events.NewMessage(pattern='^(?i)/cancel'))
+async def cancel(event):
+  global anlik_calisan
+  anlik_calisan.remove(event.chat_id)
+
+
+@client.on(events.NewMessage(pattern="^/all ?(.*)"))
+async def mentionall(event):
+  global anlik_calisan
+  if event.is_private:
+    return await event.respond("__**Bu əmr yalnız qrup və kanallarda işlədilə bilər**❗__")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond("__**Yalnız adminlər bu əmrdən isdifadə edə bilər**❗__")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+        return await event.respond("__**Köhnə mesajlar üçün userləri tag edə bilmərəm ❗ (bu mesaj məni qrupa əlavə etməmişdən qabaq yazılıb)**__")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("__**Mənə bir arqument ver**❗__")
+  else:
+    return await event.respond("__**Bir mesajı yanıtlayın və ya başqalarını tag etmək üçün mənə bir mətin verin**❗__")
+    
+  if mode == "text_on_cmd":
+    anlik_calisan.append(event.chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+      if event.chat_id not in anlik_calisan:
+        await event.respond("**Tag uğurlu şəkildə dayandırıldı** 😁")
+        return
+      if usrnum == 5:
+        await client.send_message(event.chat_id, f"{usrtxt}\n{msg}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+        
+  
+  if mode == "text_on_reply":
+    anlik_calisan.append(event.chat_id)
+ 
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+      if event.chat_id not in anlik_calisan:
+        await event.respond("**Tag uğurlu şəkildə dayandırıldı** 😻")
+        return
+      if usrnum == 5:
+        await client.send_message(event.chat_id, usrtxt, reply_to=msg)
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+
+
+@client.on(events.NewMessage(pattern="^/tektag ?(.*)"))
+async def tektag(event):
+  global elxan_tag
+  if event.is_private:
+    return await event.respond(f"**Bura Qrup Deyil🙄**")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond(f"**Yalnız Adminlər Tag Edə Bilər**🙃")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+        return await event.respond("__**Köhnə mesajları görə bilmirəm❗ (bu mesaj məni qrupa əlavə etməmişdən qabaq yazılıb**)__")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("__**Tag üçün mesajı yazmadın**❗__")
+  else:
+    return await event.respond("__**Tag etmək üçün bir mesajı yanıtlayın və ya bir mətn yazın**❗__")
+    
+  if mode == "text_on_cmd":
+    await client.send_message(event.chat_id, "🍁 **Tək-Tək Tag Başladı**\n⏱️ İnterval - 2 saniyə",
+                    buttons=(
+                      [
+                      Button.inline(f"dayandir", data="cancel")
+                      ]
+                    )
+                  ) 
+    elxan_tag.append(event.chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+      if event.chat_id not in elxan_tag:
+        await event.respond("**⛔ Tək Tək Tag Prosesi Dayandırıldı**",
+                    buttons=(
+                      [
+                      Button.inline(f"yenidən", data="yenidən")
+                      ]
+                    )
+                  )
+        return
+      if usrnum == 1:
+        await client.send_message(event.chat_id, f"{usrtxt} {msg}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+
+#########################
+
+
+@client.on(events.NewMessage(pattern=lambda x: "/tagadmin" in x.lower(), incoming=True))
+async def tag_admin(event):
+    chat = await event.get_input_chat()
+    text = "🧑‍✈️ Adminlər Tag Olundu 🧑‍✈️"
+    async for x in event.client.iter_participants(chat, 100, filter=ChannelParticipantsAdmins):
+        text += f" \n 🍁 [{x.first_name}](tg://user?id={x.id})"
+    if event.reply_to_msg_id:
+        await event.client.send_message(event.chat_id, text, reply_to=event.reply_to_msg_id)
+    else:
+        await event.reply(text)
+    raise StopPropagation
+
+def main():
+  bot.start(bot_token=TOKEN)
+  bot.run_until_disconnected()
+
+
+
+anlik_calisan = []
+
+tekli_calisan = []
+
+
+@client.on(events.NewMessage(pattern='^(?i)/cancel'))
+async def cancel(event):
+  global emoji_calisan
+  anlik_calisan.remove(event.chat_id)
+
+
+emoji = "❤️ 🧡 💛 💚 💙 💜 🖤 🤍 🤎 💝 🙂 🙃 😉 😌 😍 🥰 😘 😗 😙 😚 😋 😛 😝 😜 🤪 🤨 🧐 🤓 😎 🤩 🥳 😏 😒 🫐 🐣 🦇 🐣 🦦 🐣 🍔 🐦 🍇 🐧 🦚 🐧 🍉 🐠 🐟 " \
+        "😞 😔 😟 😕 🙁 😣 😖 😫 😩 🥺 😢 😭 😤 😠 😡  🤯 😳 🥵 🥶 😱 😨 😰 😥 😓 🤗 🤔 🤭 🤫 🤥 😶 😐 😑 😬 🙄 🐻‍❄️ 🐸 🦖 🐸 🐷 🐸 🐉 🐍 🦖 🐁 🦖 🐈‍⬛  🍩 🐏 " \
+        "😯 😦 😧 😮 😲 🥱 😴 🤤 😪 😵 🤐 🥴 🤢 🤮 🤧 😷 🤒 🤕 🤑 🤠 😈 👿 👹 👺 🤡  👻 💀 👽 👾 🤖 🎃 😺 😸 😹 ⛈️ 🌨️ ☁️ 🌦️ 🌤️ ☔ ⚡ 🌧️ 🌈 🌟 ✨ 🌟 🌒 🌓  " \
+        "😻 😼 😽 🙀 😿 🔥 😾 🌝 🌜 🌛 🌚 🙈 🙉 🙊 ❣️ 💔 🌿 🍁 🍃 🏹 🌼 ✨ 🌺 🌸 👋 🌹 👊 🌟 ⭐ 🎉 🎊 💌 💋 🫂 🌵 🌴 🍂 💐 🌻 🏵️ 💮 ⛰️ 🪨 🪵 🌪️ 🌀 🌁 🌬️ 🌊 ⛄ 🌅".split(" ")
+
+
+@client.on(events.NewMessage(pattern="^/etag ?(.*)"))
+async def mentionall(event):
+  global anlik_calisan
+  if event.is_private:
+    return await event.respond("**Bu əmr qruplar və kanallar üçün etibarlıdır**❗")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond("**Bu Əmri yalnız adminlər işlədə bilər ⚜️**")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+        return await event.respond("**bu mesaj məni qrupa əlavə etməmişdən qabaq yazılıb**")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("**Tag eləmək üçün səbəb yoxdur**🥲")
+  else:
+    return await event.respond("**Tag Prossesinə Başlamaq Üçün Səbəb Yazın**❗")
+  
+  if mode == "text_on_cmd":
+    anlik_calisan.append(event.chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{random.choice(emoji)}](tg://user?id={usr.id}) "
+      if event.chat_id not in anlik_calisan:
+        await event.respond("**Tag prosesi uğurla dayandırıldı** 🌚")
+        return
+      if usrnum == 5:
+        await client.send_message(event.chat_id, f"{usrtxt}\n{msg}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+        
+  
+  if mode == "text_on_reply":
+    anlik_calisan.append(event.chat_id)
+ 
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{random.choice(emoji)}](tg://user?id={usr.id}) "
+      if event.chat_id not in emoji_calisan:
+        await event.respond("Tag prosesi uğurla  dayandırıldı** 🌚")
+        return
+      if usrnum == 5:
+        await client.send_message(event.chat_id, usrtxt, reply_to=msg)
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+
+
+@client.on(events.NewMessage(pattern='^(?i)/cancel'))
+async def cancel(event):
+  global anlik_calisan
+  anlik_calisan.remove(event.chat_id)
+
+
+@client.on(events.NewMessage(pattern="^/tag ?(.*)"))
+async def mentionall(event):
+  global anlik_calisan
+  if event.is_private:
+    return await event.respond("**Bu əmr qruplar və kanallar üçün etibarlıdır**❗")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond("**Bu əmri yalnız adminlər işlədə bilər ⚜️**")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+        return await event.respond("Əvvəlki Mesajlara Cavab Verməyin")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("**Tag eləmək üçün səbəb yoxdur**❗")
+  else:
+    return await event.respond("**Tag Prossesinə Başlamaq Üçün Səbəb Yazın**❗")
+  
+  if mode == "text_on_cmd":
+    anlik_calisan.append(event.chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"🍁 [{usr.first_name}](tg://user?id={usr.id}) \n"
+      if event.chat_id not in anlik_calisan:
+        await event.respond("**Tag prosesi uğurla dayandırıldı** 🌚")
+        return
+      if usrnum == 5:
+        await client.send_message(event.chat_id, f"{usrtxt}\n{msg}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+ 
