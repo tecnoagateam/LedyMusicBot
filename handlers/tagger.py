dd772c658069b51a54726c359b1f8fb9097dcb8c
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-05-28 23:54:31 +0400

    Create tagger.py

---
+++ handlers/tagger.py
@@ -0,0 +1,20 @@
+
+
+
+
+async def _(event):
+    x = await eor(event, "...")
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.forward:
+            replied_user = previous_message.forward.sender_id
+        else:
+            replied_user = previous_message.sender_id
+    else:
+        await x.edit("Reply To Somone's Message")
+    user_id = replied_user
+    caption = """<a href='tg://user?id={}'>{}</a>""".format(user_id, input_str)
+    await x.edit(caption, parse_mode="HTML")
