a2886845193dbbbb34d5898b66196d7315c12e9a
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-05-29 17:20:13 +0400

    Update restart.py

--- handlers/restart.py
+++ handlers/restart.py
@@ -1,1 +1,17 @@
+from handlers import check_heroku
+from helpers.filters import command
+from pyrogram import Client, filters
+from pyrogram import Client, filters
+from helpers.decorators import authorized_users_only
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, Chat, CallbackQuery
 
+
+@Client.on_message(command(["restart"]) & ~filters.edited)
+@authorized_users_only 
+@check_heroku
+async def gib_restart(client, message, hap):
+    msg_ = await message.reply_photo(
+                                     photo="https://telegra.ph/file/e669d8ec6be16f4b7cc39.jpg", 
+                                     caption="**Yenidən işə salınma başladıldı**\n**Zəhmət olmasa gözləyin**\n**Bu, yəqin ki, 5 dəqiqə çəkəcək...**"
+   )
+    hap.restart()
