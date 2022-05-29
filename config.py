55a6728252a09e83513d56579bbfdc11c89da58b
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-05-29 09:16:07 +0400

    Update config.py

--- config.py
+++ config.py
@@ -6,14 +6,14 @@ que = {}
 admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "AgAOWWIGqnaYhdAhyPxtZwdMSlsq9dbcvCE4ClbmncigNwP2_OaNenwDr0ThnmLmaTl1s6c694fzZgXDoCTUbosfc0T9CorC8l5Po8YgbqfArqiBLrPDADV3uJAGnCcXvrM6ERr5yzJaQg6xz")
-BOT_TOKEN = list(map(int, getenv("BOT_TOKEN", "5146456073:AAFnPscOEytDNNuq8nEvch9moHVs0mFJ368", "5114978689:AAEQRpiSqgwPC9PA2JCXcKjbj_Q9cQjTBwE").split()))
+BOT_TOKEN = getenv("BOT_TOKEN", "5146456073:AAFnPscOEytDNNuq8nEvch9moHVs0mFJ368")
 BOT_NAME = getenv("BOT_NAME", "𝑳𝒆𝒅𝒚") 
 API_ID = int(getenv("API_ID", "18052289"))
 API_HASH = getenv("API_HASH", "552525f45a3066fee54ca7852235c19c")
 BOT_USERNAME = getenv("BOT_USERNAME", "SSmusicLedy_bot")
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "999"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "9999"))
 
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ . ! $ # 1 2 3 4 5 6 7 8 9 0 , Ağa ağa aga ledy Ledy").split())
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ . ! $ # 1 2 3 4 5 6 7 8 9 0 , * " ' q y a u i o p z s d f g h e j k l Z x c v b n m @ & _ - : ? ~ ` = % [ ] • | × ÷ { } ( ) ¢ € £ + Ağa ağa aga ledy Ledy").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1924693109").split()))
