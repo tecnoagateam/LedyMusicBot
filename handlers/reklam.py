62e7d14e0f9864d7e5c08782582bf71b330ffcc8
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-07 21:19:37 +0400

    Update reklam.py

--- handlers/reklam.py
+++ handlers/reklam.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Dialog, Chat, Message
 from pyrogram.errors import UserAlreadyParticipant
 
-from callsmusic.callsmusic import client as aditya
+from ledymusic.ledymusic import client as aditya
 from config import SUDO_USERS
 
 @Client.on_message(filters.command(["reklam"]))
@@ -16,15 +16,15 @@ async def broadcast(_, message: Message):
     else:
         wtf = await message.reply("`Reklam yayını başladı...`")
         if not message.reply_to_message:
-            await wtf.edit("**__mesajı yanıtlayın...__**")
+            await wtf.edit("**__Reklam üçün mesajı yanıtlayın...__**")
             return
         lmao = message.reply_to_message.text
         async for dialog in aditya.iter_dialogs():
             try:
                 await aditya.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await wtf.edit(f"`Reklam uğurla göndərildi` \n\n**Gönderildiyi yer:** `{sent}` Söhbət \n**Uğursuz oldu:** {failed} Söhbət")
+                await wtf.edit(f"`Reklam uğurla göndərilir` \n\n**Gönderildi:** `{sent}` Söhbət \n**Uğursuz:** {failed} Söhbət")
                 await asyncio.sleep(3)
             except:
                 failed=failed+1
-        await message.reply_text(f"`Reklam uğurla göndərildi' \n\n**Gönderildiyi yer:** `{sent}` Söhbət \n**Uğursuz oldu:** {failed} Söhbət")
+        await message.reply_text(f"`Reklam uğurla göndərildi' \n\n**Gönderildi:** `{sent}` Söhbət \n**Uğursuz:** {failed} Söhbət")
