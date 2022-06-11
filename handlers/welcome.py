f09333a89506445b34b259bb8fe3f2e25b7f4ddc
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-11 21:34:30 +0400

    Create welcome.py

---
+++ handlers/welcome.py
@@ -0,0 +1,53 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+@Client.on_message(filters.new_chat_members)
+async def auto_welcome(bot: Client, msg: Message):
+    first = msg.from_user.first_name
+    last = msg.from_user.last_name
+    mention = msg.from_user.mention
+    username = msg.from_user.username
+    id = msg.from_user.id
+    group_name = msg.chat.title
+    group_username = msg.chat.username
+    name_button = "🍁 Qoşul 🍁"
+    link_button = "https://t.me/SOQrup"
+    button_name = os.environ.get("WELCOME_BUTTON_NAME", name_button)
+    button_link = os.environ.get("WELCOME_BUTTON_LINK", link_button)
+    welcome_text = f"Salam, Dostum {mention} 😻 , Xoş gəldin {group_name}\nSənin ID-in : {id}"
+    WELCOME_TEXT = os.environ.get("WELCOME_TEXT", welcome_text)
+    print("Welcome Message Activate")
+    BUTTON = bool(os.environ.get("WELCOME_BUTTON"))
+    if not BUTTON:
+       await msg.reply_text(text=WELCOME_TEXT.format(
+           first = msg.from_user.first_name,
+           last = msg.from_user.last_name,
+           username = None if not msg.from_user.username else '@' + msg.from_user.username,
+           mention = msg.from_user.mention,
+           id = msg.from_user.id,
+           group_name = msg.chat.title,
+           group_username = None if not msg.chat.username else '@' + msg.chat.username
+          )
+       )
+    else:
+       await msg.reply_text(text=WELCOME_TEXT.format(
+           first = msg.from_user.first_name,
+           last = msg.from_user.last_name,
+           username = None if not msg.from_user.username else '@' + msg.from_user.username,
+           mention = msg.from_user.mention,
+           id = msg.from_user.id,
+           group_name = msg.chat.title,
+           group_username = None if not msg.chat.username else '@' + msg.chat.username
+          ),
+       reply_markup=InlineKeyboardMarkup(
+               [
+                   [
+                       InlineKeyboardButton
+                           (
+                               button_name, url=button_link
+                           )
+                   ]  
+               ]
+           )
+       )  
