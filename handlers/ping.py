3568b681e1cd63fd591dfa9b776efee9b59bd0cf
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-11 23:01:31 +0400

    Update ping.py

--- handlers/ping.py
+++ handlers/ping.py
@@ -4,6 +4,7 @@ from config import BOT_USERNAME
 from helpers.filters import command
 from pyrogram import Client, filters
 
+
 @Client.on_message(command(["ping", f"ping@{BOT_USERNAME}"]))
 async def ping_pong(client: Client, message: Message):
     start = time()
