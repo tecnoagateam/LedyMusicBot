59140edc6e840f0c0905e543bbf73c7d8866e181
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-11 23:08:48 +0400

    Update ytaxtar.py

--- handlers/ytaxtar.py
+++ handlers/ytaxtar.py
@@ -1,40 +1,52 @@
-# Size linkler halinde arar. 
 import logging
 
-from config import BOT_USERNAME
-from pyrogram.types import Message
-from search_engine_parser import GoogleSearch
+from config import BOT_USERNAME as bn
+from helpers.filters import command
+from pyrogram import Client
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+)
 from youtube_search import YoutubeSearch
-
-from pyrogram import Client as app, filters
 
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger(__name__)
-
-import pyrogram
-
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-@app.on_message(pyrogram.filters.command(["axtar", f"axtar@{BOT_USERNAME}"]))
+
+@Client.on_message(command(["axtar", f"axtar@{bn}"]))
 async def ytsearch(_, message: Message):
+    
+    keyboard = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "🗑 Bağla", callback_data="close",
+                )
+            ]
+        ]
+    )
+    
     try:
         if len(message.command) < 2:
-            await message.reply_text("Axtarmaq üçün mənə Musiqi adı verin!")
+            await message.reply_text("Axtarmaq üçün Mənə Bir Arqument ver!**")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("Axtarıram....")
-        results = YoutubeSearch(query, max_results=4).to_dict()
+        m = await message.reply_text("🔎 **Axtarıram...**")
+        results = YoutubeSearch(query, max_results=5).to_dict()
         i = 0
         text = ""
-        while i < 4:
-            text += f"Ad - {results[i]['title']}\n"
-            text += f"Vaxt - {results[i]['duration']}\n"
-            text += f"İzlənmə - {results[i]['views']}\n"
-            text += f"Kanal - {results[i]['channel']}\n"
-            text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
+        while i < 5:
+            text += f"🏷 **Ad:** __{results[i]['title']}__\n"
+            text += f"⏱ **Müddət:** `{results[i]['duration']}`\n"
+            text += f"👀 **İzlənmə:** `{results[i]['views']}`\n"
+            text += f"📣 **Kanal:** {results[i]['channel']}\n"
+            text += f"🔗: https://www.youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
-        await m.edit(text, disable_web_page_preview=True)
+        await m.edit(text, reply_markup=keyboard, disable_web_page_preview=True)
     except Exception as e:
-        await message.reply_text(str(e))
+        await m.edit(str(e))
