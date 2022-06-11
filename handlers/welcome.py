5b4f661048c4ca19570c5770beee94d21cb88030
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-11 21:44:03 +0400

    Update welcome.py

--- handlers/welcome.py
+++ handlers/welcome.py
@@ -15,7 +15,7 @@ async def auto_welcome(bot: Client, msg: Message):
     link_button = "https://t.me/SOQrup"
     button_name = os.environ.get("WELCOME_BUTTON_NAME", name_button)
     button_link = os.environ.get("WELCOME_BUTTON_LINK", link_button)
-    welcome_text = f"Salam, Dostum {mention} 😻 , Xoş gəldin {group_name}\nSənin ID-in : {id}"
+    welcome_text = f"Salam, {mention}, {group_name} Xoş gəldin! Səni aramızda görməyimə şadam.❤️\n\nSənin ID-in : {id}"
     WELCOME_TEXT = os.environ.get("WELCOME_TEXT", welcome_text)
     print("Welcome Message Activate")
     BUTTON = bool(os.environ.get("WELCOME_BUTTON"))
