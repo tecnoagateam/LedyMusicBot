fd3234c5f0fe3bad99fe6de539eb5804b5922fa2
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-11 16:35:20 +0400

    Update assistant.py

--- handlers/assistant.py
+++ handlers/assistant.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import UserAlreadyParticipant
 from helpers.decorators import errors, authorized_users_only
-from config import BOT_USERNAME
+from config import BOT_USERNAME, SUDO_USERS
 
 @Client.on_message(filters.group & filters.command(["assistantqosul", f"assistantqosul@{BOT_USERNAME}", "qatil"]))
 @authorized_users_only
