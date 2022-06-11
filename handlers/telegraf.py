6b2f9bee75c84bc431037b1f80ab7a7ee42809bf
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-11 21:58:20 +0400

    Create telegraf.py

---
+++ handlers/telegraf.py
@@ -0,0 +1,53 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from telegraph import upload_file
+
+DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
+
+@Client.on_message(filters.private & filters.media)
+async def getmedia(bot, update):
+    medianame = DOWNLOAD_LOCATION + str(update.from_user.id)
+    try:
+        message = await update.reply(
+            text="`Processing Media you Send...`",
+            quote=True,
+            disable_web_page_preview=True
+        )
+        await bot.download_media(
+            message=update,
+            file_name=medianame
+        )
+        response = upload_file(medianame)
+        try:
+            os.remove(medianame)
+        except:
+            pass
+    except Exception as error:
+        print(error)
+        text=f"Xəta :- <code>{error}</code>"
+        reply_markup=InlineKeyboardMarkup(
+            [[
+            InlineKeyboardButton('More Help', url=https://t.me/SOQrup')
+            ]]
+        )
+        await message.edit_text(
+            text=text,
+            disable_web_page_preview=True,
+            reply_markup=reply_markup
+        )
+        return
+    text=f"**Link :-** `https://telegra.ph{response[0]}`"
+    reply_markup=InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton(text="Link-i aç", url=f"https://telegra.ph{response[0]}"),
+        InlineKeyboardButton(text="Paylaş", url=f"https://telegram.me/share/url?url=https://telegra.ph{response[0]}")
+        ],[
+        InlineKeyboardButton(text="Dəstək Chat", url="https://telegram.me/SOQrup") 
+        ]]
+    )
+    await message.edit_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )
