f7c8c69cdb1b62cd8e29688116e8d8bd44f5d19a
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-12 16:32:12 +0400

    Update ping.py

--- handlers/ping.py
+++ handlers/ping.py
@@ -49,3 +49,25 @@ async def ping(client, message):
    uptime_sec = (current_time - START_TIME).total_seconds()
    uptime = await _human_time_duration(int(uptime_sec))
    await m_reply.edit(f"🏓 **Pong!!** `{delta_ping * 1000:.3f} ms`\n\n**Uptime** - `{uptime}`")
+
+from time import time
+from config import BOT_USERNAME
+
+from helpers.filters import command
+from pyrogram import Client, filters
+
+
+@Client.on_message(command(["ping", f"ping@{BOT_USERNAME}"]))
+async def ping_pong(client, message):
+    start = time()
+    m_reply = await message.reply_text("__pinging...__")
+    delta_ping = time() - start
+    await m_reply.edit("**0% ▒▒▒▒▒▒▒▒▒▒**")
+    await m_reply.edit("**20% ██▒▒▒▒▒▒▒▒**")
+    await m_reply.edit("**40% ████▒▒▒▒▒▒**")
+    await m_reply.edit("**60% ██████▒▒▒▒**")
+    await m_reply.edit("**80% ████████▒▒**")
+    await m_reply.edit("**100% ██████████**")
+    await asyncio.sleep(1)
+    await m_reply.edit_text("🏓 `PONG!!`\n" f"⚡️ `{delta_ping * 1000:.3f} ms`")
+
