0bcac4bd651e73e8d48d8a4da4c9b99f03269aad
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-12 21:33:19 +0400

    Create uptime.py

---
+++ handlers/uptime.py
@@ -0,0 +1,41 @@
+from time import time
+from datetime import datetime
+from pyrogram import Client, filters
+from config import USERNAME_BOT
+from helpers.decorators import sudo_users_only
+from helpers.filters import command
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ("week", 60 * 60 * 24 * 7),
+    ("days", 60 * 60 * 24),
+    ("h", 60 * 60),
+    ("m", 60),
+    ("s", 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+
+
+@Client.on_message(filters.command(["uptime", f"uptime@{USERNAME_BOT}"]))
+@sudo_users_only
+async def get_uptime(client: Client, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await message.reply_text(
+        "**Bot Status:\n"
+        f"• **uptime:** `{uptime}`\n"
+        f"• **start time:** `{START_TIME_ISO}`"
+    )
