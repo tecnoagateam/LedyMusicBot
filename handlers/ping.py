3922059905b93cd574e259b0e747bb509afc0ebe
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-12 16:01:08 +0400

    Update ping.py

--- handlers/ping.py
+++ handlers/ping.py
@@ -10,4 +10,11 @@ async def ping_pong(client, message):
     start = time()
     m_reply = await message.reply_text("__pinging...__")
     delta_ping = time() - start
+    await m_reply.edit("**0% ▒▒▒▒▒▒▒▒▒▒**")
+    await m_reply.edit("**20% ██▒▒▒▒▒▒▒▒**")
+    await m_reply.edit("**40% ████▒▒▒▒▒▒**")
+    await m_reply.edit("**60% ██████▒▒▒▒**")
+    await m_reply.edit("**80% ████████▒▒**")
+    await m_reply.edit("**100% ██████████**")
+    await asyncio.sleep(1)
     await m_reply.edit_text("🏓 `PONG!!`\n" f"⚡️ `{delta_ping * 1000:.3f} ms`")
