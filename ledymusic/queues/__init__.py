2e497711c0d10cc3f9f3b1f789484c00caa4018f
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-12 14:18:14 +0400

    Update __init__.py

--- ledymusic/queues/__init__.py
+++ ledymusic/queues/__init__.py
@@ -1,1 +1,7 @@
-from .queues import put, get, is_empty, task_done, clear
+from ledymusic.queues.queues import clear 
+from ledymusic.queues.queues import get
+from ledymusic.queues.queues import is_empty
+from ledymusic.queues.queues import put
+from ledymusic.queues.queues import task_done
+
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
