6f4940c571fc0df6888ea436429c824093821de2
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-13 18:21:45 +0400

    Update ping.py

--- handlers/ping.py
+++ handlers/ping.py
@@ -3,6 +3,7 @@ from config import BOT_USERNAME
 
 from helpers.filters import command
 from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
 
 
