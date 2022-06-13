d37d87682e05cdc9e2e26e7211a19a40bea5ad01
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-13 19:01:53 +0400

    Update decorators.py

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -31,6 +31,14 @@ def authorized_users_only(func: Callable) -> Callable:
     return decorator
 
 
+def sudo_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in SUDO_USERS:
+            return await func(client, message)
+
+    return decorator
+
+
 # Utils Helper
 def humanbytes(size):
     """İnsanların oxuya bilməsi üçün baytları bayta çevirin"""
