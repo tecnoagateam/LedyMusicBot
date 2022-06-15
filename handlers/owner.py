bb72500022b0ad1391ef7f1a7952a7460587f63b
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-15 21:18:40 +0400

    Update owner.py

--- handlers/owner.py
+++ handlers/owner.py
@@ -35,7 +35,6 @@ async def botstats(_, message: Message):
     cpu_usage = psutil.cpu_percent()
     ram_usage = psutil.virtual_memory().percent
     disk_usage = psutil.disk_usage("/").percent
-    total_users = await db.total_users_count()
     await message.reply_text(
         text=f"**📊 stats @{BOT_USERNAME}** \n\n**🤖 bot version:** `V.1.2` \n\n**💾 disk usage:** \n » **disk space:** `{total}` \n » **used:** `{used}({disk_usage}%)` \n » **free:** `{free}` \n\n**🎛 hardware usage:** \n » **CPU usage:** `{cpu_usage}%` \n » **RAM usage:** `{ram_usage}%`",
         parse_mode="Markdown",
