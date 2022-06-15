cc22790bfd7f0555381cb1b41e18f273ff1c351e
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-15 21:17:34 +0400

    Create owner.py

---
+++ handlers/owner.py
@@ -0,0 +1,211 @@
+import os
+import shutil
+import sys
+import traceback
+from functools import wraps
+from os import environ, execle
+
+import heroku3
+import psutil
+from config import (
+    BOT_USERNAME,
+    HEROKU_API_KEY,
+    HEROKU_APP_NAME,
+    HEROKU_URL,
+    OWNER_ID,
+    U_BRANCH,
+    UPSTREAM_REPO,
+)
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError
+from helpers.decorators import sudo_users_only, humanbytes
+from helpers.filters import command
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+
+# Stats Of Your Bot
+@Client.on_message(command("stats"))
+@sudo_users_only
+async def botstats(_, message: Message):
+    total, used, free = shutil.disk_usage(".")
+    total = humanbytes(total)
+    used = humanbytes(used)
+    free = humanbytes(free)
+    cpu_usage = psutil.cpu_percent()
+    ram_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage("/").percent
+    total_users = await db.total_users_count()
+    await message.reply_text(
+        text=f"**📊 stats @{BOT_USERNAME}** \n\n**🤖 bot version:** `V.1.2` \n\n**💾 disk usage:** \n » **disk space:** `{total}` \n » **used:** `{used}({disk_usage}%)` \n » **free:** `{free}` \n\n**🎛 hardware usage:** \n » **CPU usage:** `{cpu_usage}%` \n » **RAM usage:** `{ram_usage}%`",
+        parse_mode="Markdown",
+        quote=True,
+    )
+
+
+
+# ====== UPDATER ======
+
+REPO_ = UPSTREAM_REPO
+BRANCH_ = U_BRANCH
+
+
+@Client.on_message(command("update") & filters.user(OWNER_ID))
+async def updatebot(_, message: Message):
+    msg = await message.reply_text("**updating bot, please wait for a while...**")
+    try:
+        repo = Repo()
+    except GitCommandError:
+        return await msg.edit("**invalid git command !**")
+    except InvalidGitRepositoryError:
+        repo = Repo.init()
+        if "upstream" in repo.remotes:
+            origin = repo.remote("upstream")
+        else:
+            origin = repo.create_remote("upstream", REPO_)
+        origin.fetch()
+        repo.create_head(U_BRANCH, origin.refs.main)
+        repo.heads.main.set_tracking_branch(origin.refs.main)
+        repo.heads.main.checkout(True)
+    if repo.active_branch.name != U_BRANCH:
+        return await msg.edit(
+            f"**sorry, you are using costum branch named:** `{repo.active_branch.name}`!\n\nchange to `{U_BRANCH}` branch to continue update!"
+        )
+    try:
+        repo.create_remote("upstream", REPO_)
+    except BaseException:
+        pass
+    ups_rem = repo.remote("upstream")
+    ups_rem.fetch(U_BRANCH)
+    if not HEROKU_URL:
+        try:
+            ups_rem.pull(U_BRANCH)
+        except GitCommandError:
+            repo.git.reset("--hard", "FETCH_HEAD")
+        await run_cmd("pip3 install --no-cache-dir -r requirements.txt")
+        await msg.edit("**update finished, restarting now...**")
+        args = [sys.executable, "ledy.py"]
+        execle(sys.executable, *args, environ)
+        sys.exit()
+        return
+    else:
+        await msg.edit("`heroku detected!`")
+        await msg.edit(
+            "`updating and restarting is started, please wait for 5-10 minutes!`"
+        )
+        ups_rem.fetch(U_BRANCH)
+        repo.git.reset("--hard", "FETCH_HEAD")
+        if "heroku" in repo.remotes:
+            remote = repo.remote("heroku")
+            remote.set_url(HEROKU_URL)
+        else:
+            remote = repo.create_remote("heroku", HEROKU_URL)
+        try:
+            remote.push(refspec="HEAD:refs/heads/main", force=True)
+        except BaseException as error:
+            await msg.edit(f"🚫 **updater error** \n\nTraceBack : `{error}`")
+            return repo.__del__()
+
+
+# HEROKU LOGS
+
+
+async def edit_or_send_as_file(
+    text: str,
+    message: Message,
+    client: Client,
+    caption: str = "`Result!`",
+    file_name: str = "result",
+    parse_mode="md",
+):
+    """Send As File If Len Of Text Exceeds Tg Limit Else Edit Message"""
+    if not text:
+        await message.edit("`there is something other than text, aborting...`")
+        return
+    if len(text) <= 1024:
+        return await message.edit(text, parse_mode=parse_mode)
+
+    await message.edit("`output is too large, sending as file!`")
+    file_names = f"{file_name}.text"
+    open(file_names, "w").write(text)
+    await client.send_document(message.chat.id, file_names, caption=caption)
+    await message.delete()
+    if os.path.exists(file_names):
+        os.remove(file_names)
+    return
+
+
+heroku_client = heroku3.from_key(HEROKU_API_KEY) if HEROKU_API_KEY else None
+
+
+def _check_heroku(func):
+    @wraps(func)
+    async def heroku_cli(client, message):
+        heroku_app = None
+        if not heroku_client:
+            await message.reply_text("`please add heroku api key to use this feature!`")
+        elif not HEROKU_APP_NAME:
+            await edit_or_reply(
+                message, "`please add heroku app name to use this feature!`"
+            )
+        if HEROKU_APP_NAME and heroku_client:
+            try:
+                heroku_app = heroku_client.app(HEROKU_APP_NAME)
+            except:
+                await message.reply_text(
+                    message,
+                    "`heroku api key and app name doesn't match, please recheck`",
+                )
+            if heroku_app:
+                await func(client, message, heroku_app)
+
+    return heroku_cli
+
+
+@Client.on_message(command("logs"))
+@sudo_users_only
+@_check_heroku
+async def logswen(client: Client, message: Message, happ):
+    msg = await message.reply_text("`please wait for a moment!`")
+    logs = happ.get_log()
+    capt = f"Heroku logs of `{HEROKU_APP_NAME}`"
+    await edit_or_send_as_file(logs, msg, client, capt, "logs")
+
+
+# Set Heroku Var
+@Client.on_message(command("setvar") & filters.user(OWNER_ID))
+@_check_heroku
+async def setvar(client: Client, message: Message, app_):
+    msg = await message.reply_text(message, "`please wait...`")
+    heroku_var = app_.config()
+    _var = get_text(message)
+    if not _var:
+        await msg.edit("**usage:** `/setvar (var) (value)`")
+        return
+    if " " not in _var:
+        await msg.edit("**usage:** `/setvar (var) (value)`")
+        return
+    var_ = _var.split(" ", 1)
+    if len(var_) > 2:
+        await msg.edit("**usage:** `/setvar (var) (value)`")
+        return
+    _varname, _varvalue = var_
+    await msg.edit(f"**variable:** `{_varname}` \n**new value:** `{_varvalue}`")
+    heroku_var[_varname] = _varvalue
+
+
+# Delete Heroku Var
+@Client.on_message(command("delvar") & filters.user(OWNER_ID))
+@_check_heroku
+async def delvar(client: Client, message: Message, app_):
+    msg = await message.reply_text(message, "`please wait...!`")
+    heroku_var = app_.config()
+    _var = get_text(message)
+    if not _var:
+        await msg.edit("`give a var name to delete!`")
+        return
+    if _var not in heroku_var:
+        await msg.edit("`this var doesn't exists!`")
+        return
+    await msg.edit(f"sucessfully deleted var `{_var}`")
+    del heroku_var[_var]
