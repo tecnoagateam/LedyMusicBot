1f34e7841bb4bcfa094de126d0d38d76923655e9
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-15 23:16:44 +0400

    Update config.py

--- config.py
+++ config.py
@@ -29,10 +29,10 @@ SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1924693109").split()))
 #  ------------------------------------------------------------------------------------------------------------ 
 #    Yeni güncellemede...
 
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "ledy")
+STREAM_BRANCH = getenv("STREAM_BRANCH", "ledy")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", none) 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", none) 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/AzeMusic/LedyMusicBot") 
+STREAM_REPO = getenv("STREAM_REPO", "https://github.com/AzeMusic/LedyMusicBot") 
 HEROKU_URL = fetch_heroku_git_url(HEROKU_API_KEY, HEROKU_APP_NAME)
 
 # DB_URL new mode -------ledyservic------
