328ba208fc0312e93b4a1db1b150ffcbfe9145bf
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-16 12:57:11 +0400

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1924693109").split()))
 
 # DB_URL new mode -------ledyservic------
 
-DATABASE_URL = getenv("DATABASE_URL")
+DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
 
 # Log kanalı.... 
 LOG_CHANNEL = getenv("LOG_CHANNEL")
