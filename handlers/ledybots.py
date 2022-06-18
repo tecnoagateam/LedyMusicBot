c128a6f57102e99b3df44e8324772b975a9faa2e
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-18 09:03:55 +0400

    Update ledybots.py

--- handlers/ledybots.py
+++ handlers/ledybots.py
@@ -1,1 +1,9 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
+from config import BOT_USERNAME, BOT_NAME as bot
+from helpers.filters import command, other_filters2
+
+
+
+ 
