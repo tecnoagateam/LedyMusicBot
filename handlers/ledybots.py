6cf6dd1db2c29a1b2fcab796f6ab59341ef821e6
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-18 09:08:55 +0400

    Update ledybots.py

--- handlers/ledybots.py
+++ handlers/ledybots.py
@@ -7,3 +7,12 @@ from helpers.filters import command, other_filters2
 
 
  
+@Client.on_message(command(["ledybots", f"ledybots@{BOT_USERNAME}"]))
+async def start(_, message: Message):
+                await message.reply_photo(
+                "",
+                caption=(f"""✧═══════•❀•═══════✧
+**Salam {message.from_user.mention}!\n\nLedy Botlar ən sürətli və güclü telegram botlarıdır.\n\nSən Onlardan Rahat və təhlükəsiz istifadə edə bilərsən!    ✧═══════•❀•═══════✧
+"""),
+         reply_markup=InlineKeyboardMarkup(
+            [
