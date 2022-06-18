d76f0e8b338cfcf760141b9f6304c8ca1caa209d
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-18 08:25:33 +0400

    Update restart.py

--- handlers/restart.py
+++ handlers/restart.py
@@ -3,7 +3,7 @@ from helpers.filters import command
 from pyrogram import Client, filters
 from pyrogram import Client, filters
 from helpers.decorators import sudo_users_only
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, Chat, CallbackQuery
+from pyrogram.types import Message
 
 
 @Client.on_message(command(["restart"]) & ~filters.edited)
