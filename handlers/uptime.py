bd70ba22000dc969b6960ba440c022c6d49e0fd1
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-18 14:53:13 +0400

    Update uptime.py

--- handlers/uptime.py
+++ handlers/uptime.py
@@ -12,8 +12,8 @@ TIME_DURATION_UNITS = (
     ("hefte", 60 * 60 * 24 * 7),
     ("gun", 60 * 60 * 24),
     ("saat", 60 * 60),
-    ("deqiqe", 60),
-    ("saniye", 1)
+    ("deq", 60),
+    ("san", 1)
 )
 
 async def _human_time_duration(seconds):
@@ -24,7 +24,7 @@ async def _human_time_duration(seconds):
         amount, seconds = divmod(int(seconds), div)
         if amount > 0:
             parts.append('{} {}{}'
-                         .format(amount, unit, "" if amount == 1 else "s"))
+                         .format(amount, unit, "" if amount == 1 else "i"))
     return ', '.join(parts)
 
 
