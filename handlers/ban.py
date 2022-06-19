04aab58868b91abe24728cf3a8b3eb7d63d0db7b
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-19 13:12:23 +0400

    Create ban.py

---
+++ handlers/ban.py
@@ -0,0 +1,34 @@
+ #əbədilik ban 👋👋
+
+import os
+from config import BOT_USERNAME
+from pyrogram import Client, filters
+from pyrogram.types import Message, User
+
+
+@Client.on_message(filters.command(["ban", f"ban@{BOT_USERNAME}"]))
+async def ban(bot, message):
+    chatid = message.chat.id
+    if message.reply_to_message:
+        admins_list = await bot.get_chat_members(
+            chat_id=chatid, filter="administrators"
+        )
+        admins = []
+        for admin in admins_list:
+            id = admin.user.id
+            admins.append(id)
+        userid = message.from_user.id
+        if userid in admins:
+            user_to_ban = message.reply_to_message.from_user.id
+            if user_to_ban in admins:
+                await message.reply(text="Think he is Admin, Can't Ban Admins")
+            else:
+                try:
+                    await bot.ban_chat_member(chat_id=chatid, user_id=user_to_ban)
+                    await message.reply_text(
+                        f"**Aha Biri Daha Getdi**\n {message.reply_to_message.from_user.mention} xoş getdin! 👋"
+                    )
+                except Exception as error:
+                    await message.reply_text(f"{error}")
+        else:
+            await message.reply_text("Nice try, But wrong move..")
