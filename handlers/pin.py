f2dbda498aa0952486cf190e56e952a66268d6cf
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-20 10:33:18 +0400

    Update pin.py

--- handlers/pin.py
+++ handlers/pin.py
@@ -1,11 +1,13 @@
 from config import BOT_USERNAME
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from helpers.decorators import authorized_users_only, sudo_users_only
+from helpers.filters import command
 
 
 @Client.on_message(command(["pin", f"pin@{BOT_USERNAME}"]))
 @sudo_users_only
-@auth_users_only
+@authorized_users_only
 async def pin(_, message: Message):
     if not message.reply_to_message:
         return
@@ -14,7 +16,7 @@ async def pin(_, message: Message):
 
 @Client.on_message(command(["unpin", f"unpin@{BOT_USERNAME}"]))
 @sudo_users_only
-@auth_users_only
+@authorized_users_only
 async def unpin(_, message: Message):
     if not message.reply_to_message:
         return
