2b2b11831dd9dca3e8f3f41bc90535c47cbbd5be
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-21 21:55:50 +0400

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -186,7 +186,7 @@ async def owner(_, query: CallbackQuery):
 
 @Client.on_callback_query(filters.regex("tagger"))
 async def tagger(_, query: CallbackQuery):
-    await query.edit_message_text(f"""<b>Salam {query.from_user.mention}!\nBu Botun Userləri tağ etmək üçün Əmrlər Menyusu:\n\n 🦅 /ledytag - Userləri Normal Şəkildə tağ edər\nBot Və silinmiş hesablardan başqa!\n ❌ /stop - tağ prosesini dayandırar\n 🎖️ /staff - Qrup staffı adminləri tağ edər/n 🗑️ /sil - Silinmiş hesabları Qrupdan çixarar/n 🤖 /bots - Qrup Bot siyahısını göstərər.\n\n</b>""",
+    await query.edit_message_text(f"""<b>Salam {query.from_user.mention}!\nBu Botun Userləri tağ etmək üçün Əmrlər Menyusu:\n\n 🦅 /ledytag - Userləri Normal Şəkildə tağ edər\nBot Və silinmiş hesablardan başqa!\n ❌ /stop - tağ prosesini dayandırar\n🎖️ /staff - Qrup staffı adminləri tağ edər/n🗑️ /sil - Silinmiş hesabları Qrupdan çixarar/n🤖 /bots - Qrup Bot siyahısını göstərər.\n\n</b>""",
     reply_markup=InlineKeyboardMarkup(
              [
                  [
