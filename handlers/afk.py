0cfd65758ae93024870d9538b1069a5404ae7ed2
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-21 09:22:26 +0400

    Update afk.py

--- handlers/afk.py
+++ handlers/afk.py
@@ -15,18 +15,18 @@ async def afk_handler(client, message):
         afk_time = end - start
         if message.from_user.is_bot is False:
             await message.reply_text(
-                f"<b>Afk {afk_time}</b>\n" f"<b>Sənə :</b> <i>{reason}</i>"
+                f"<b>Afk {afk_time}</b>\n" f"<b>Səbəb :</b> <i>{reason}</i>"
             )
     except NameError:
         pass
 
 
-@Client.on_message(filters.command("afk", f"afk@{BOT_USERNAME}") & filters.me)
+@Client.on_message(filters.command("afk", f"afk@{BOT_USERNAME}"))
 async def afk(client, message):
     global start, end, handler, reason
     start = datetime.datetime.now().replace(microsecond=0)
     handler = client.add_handler(
-        MessageHandler(afk_handler, (filters.public & ~filters.me))
+        MessageHandler(afk_handler, (filters.public))
     )
     if len(message.text.split()) >= 2:
         reason = message.text.split(" ", maxsplit=1)[1]
