76458868185e1f07eaf058486dc0edd5f4bc47a3
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-21 15:16:13 +0400

    Update ledytag.py

--- handlers/ledytag.py
+++ handlers/ledytag.py
@@ -1,27 +1,220 @@
-import asyncio
-
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from helpers.filters import command
-from helpers.decorators import authorized_users_only
-from config import BOT_USERNAME
+import os
+import asyncio
+from pyrogram import enums
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.errors import FloodWait
 
 
-@Client.on_message(command(["ledytag", f"ledytag@{BOT_USERNAME}"]))
-@authorized_users_only
-async def ledytag(client, message):
-    await message.delete()
-    chat_id = message.chat.id
-    string = ""
-    limit = 1
-    icm = client.iter_chat_members(chat_id)
-    async for member in icm:
-        tag = member.user.username
-        if limit <= 5:
-            string += f"🦅 @{tag}\n" if tag != None else f"🦅 {member.user.mention}\n"
-            limit += 1
+
+chatQueue = []
+
+stopProcess = False
+
+@teletips.on_message(filters.command(["ledytag","ledytag@SSmusicledy_bot"]))
+async def everyone(client, message):
+  global stopProcess
+  try: 
+    try:
+      sender = await teletips.get_chat_member(message.chat.id, message.from_user.id)
+      has_permissions = sender.privileges
+    except:
+      has_permissions = message.sender_chat  
+    if has_permissions:
+      if len(chatQueue) > 5:
+        await message.reply("⛔️ | I'm already working on my maximum number of 5 chats at the moment. Please try again shortly.")
+      else:  
+        if message.chat.id in chatQueue:
+          await message.reply("🚫 | There's already an ongoing process in this chat. Please /stop to start a new one.")
+        else:  
+          chatQueue.append(message.chat.id)
+          if len(message.command) > 1:
+            inputText = message.command[1]
+          elif len(message.command) == 1:
+            inputText = ""    
+          membersList = []
+          async for member in teletips.get_chat_members(message.chat.id):
+            if member.user.is_bot == True:
+              pass
+            elif member.user.is_deleted == True:
+              pass
+            else:
+              membersList.append(member.user)
+          i = 0
+          lenMembersList = len(membersList)
+          if stopProcess: stopProcess = False
+          while len(membersList) > 0 and not stopProcess :
+            j = 0
+            text1 = f"{inputText}\n\n"
+            try:    
+              while j < 10:
+                user = membersList.pop(0)
+                if user.username == None:
+                  text1 += f"{user.mention} "
+                  j+=1
+                else:
+                  text1 += f"@{user.username} "
+                  j+=1
+              try:     
+                await teletips.send_message(message.chat.id, text1)
+              except Exception:
+                pass  
+              await asyncio.sleep(10) 
+              i+=10
+            except IndexError:
+              try:
+                await teletips.send_message(message.chat.id, text1)  
+              except Exception:
+                pass  
+              i = i+j
+          if i == lenMembersList:    
+            await message.reply(f"✅ | Successfully mentioned **total number of {i} members**.\n❌ | Bots and deleted accounts were rejected.") 
+          else:
+            await message.reply(f"✅ | Successfully mentioned **{i} members.**\n❌ | Bots and deleted accounts were rejected.")    
+          chatQueue.remove(message.chat.id)
+    else:
+      await message.reply("👮🏻 | Sorry, **only admins** can execute this command.")  
+  except FloodWait as e:
+    await asyncio.sleep(e.value) 
+
+@teletips.on_message(filters.command(["remove","sil", "remove@SSmusicledy_bot", "sil@SSmusicledy_bot"]))
+async def remove(client, message):
+  global stopProcess
+  try: 
+    try:
+      sender = await teletips.get_chat_member(message.chat.id, message.from_user.id)
+      has_permissions = sender.privileges
+    except:
+      has_permissions = message.sender_chat  
+    if has_permissions:
+      bot = await teletips.get_chat_member(message.chat.id, "self")
+      if bot.status == ChatMemberStatus.MEMBER:
+        await message.reply("🕹 | I need admin permissions to remove deleted accounts.")  
+      else:  
+        if len(chatQueue) > 5 :
+          await message.reply("⛔️ | I'm already working on my maximum number of 5 chats at the moment. Please try again shortly.")
+        else:  
+          if message.chat.id in chatQueue:
+            await message.reply("🚫 | There's already an ongoing process in this chat. Please /stop to start a new one.")
+          else:  
+            chatQueue.append(message.chat.id)  
+            deletedList = []
+            async for member in teletips.get_chat_members(message.chat.id):
+              if member.user.is_deleted == True:
+                deletedList.append(member.user)
+              else:
+                pass
+            lenDeletedList = len(deletedList)  
+            if lenDeletedList == 0:
+              await message.reply("👻 | No deleted accounts in this chat.")
+              chatQueue.remove(message.chat.id)
+            else:
+              k = 0
+              processTime = lenDeletedList*10
+              temp = await teletips.send_message(message.chat.id, f"🚨 | Total of {lenDeletedList} deleted accounts has been detected.\n⏳ | Estimated time: {processTime} seconds from now.")
+              if stopProcess: stopProcess = False
+              while len(deletedList) > 0 and not stopProcess:   
+                deletedAccount = deletedList.pop(0)
+                try:
+                  await teletips.ban_chat_member(message.chat.id, deletedAccount.id)
+                except Exception:
+                  pass  
+                k+=1
+                await asyncio.sleep(10)
+              if k == lenDeletedList:  
+                await message.reply(f"✅ | Successfully removed all deleted accounts from this chat.")  
+                await temp.delete()
+              else:
+                await message.reply(f"✅ | Successfully removed {k} deleted accounts from this chat.")  
+                await temp.delete()  
+              chatQueue.remove(message.chat.id)
+    else:
+      await message.reply("👮🏻 | Sorry, **only admins** can execute this command.")  
+  except FloodWait as e:
+    await asyncio.sleep(e.value)                               
+        
+@teletips.on_message(filters.command(["stop","cancel", "stop@SSmusicledy_bot", "cancel@SSmusicledy_bot"]))
+async def stop(client, message):
+  global stopProcess
+  try:
+    try:
+      sender = await teletips.get_chat_member(message.chat.id, message.from_user.id)
+      has_permissions = sender.privileges
+    except:
+      has_permissions = message.sender_chat  
+    if has_permissions:
+      if not message.chat.id in chatQueue:
+        await message.reply("🤷🏻‍♀️ | There is no ongoing process to stop.")
+      else:
+        stopProcess = True
+        await message.reply("🛑 | Stopped.")
+    else:
+      await message.reply("👮🏻 | Sorry, **only admins** can execute this command.")
+  except FloodWait as e:
+    await asyncio.sleep(e.value)
+
+@teletips.on_message(filters.command(["admins","staff", "admins@SSmusicledy_bot", "staff@SSmusicledy_bot"]))
+async def admins(client, message):
+  try: 
+    adminList = []
+    ownerList = []
+    async for admin in teletips.get_chat_members(message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+      if admin.privileges.is_anonymous == False:
+        if admin.user.is_bot == True:
+          pass
+        elif admin.status == ChatMemberStatus.OWNER:
+          ownerList.append(admin.user)
+        else:  
+          adminList.append(admin.user)
+      else:
+        pass   
+    lenAdminList= len(ownerList) + len(adminList)  
+    text2 = f"**GROUP STAFF - {message.chat.title}**\n\n"
+    try:
+      owner = ownerList[0]
+      if owner.username == None:
+        text2 += f"👑 Owner\n└ {owner.mention}\n\n👮🏻 Admins\n"
+      else:
+        text2 += f"👑 Owner\n└ @{owner.username}\n\n👮🏻 Admins\n"
+    except:
+      text2 += f"👑 Owner\n└ <i>Hidden</i>\n\n👮🏻 Admins\n"
+    if len(adminList) == 0:
+      text2 += "└ <i>Admins are hidden</i>"  
+      await teletips.send_message(message.chat.id, text2)   
+    else:  
+      while len(adminList) > 1:
+        admin = adminList.pop(0)
+        if admin.username == None:
+          text2 += f"├ {admin.mention}\n"
         else:
-            await client.send_message(chat_id, text=string)
-            limit = 1
-            string = ""
-            await asyncio.sleep(2)
+          text2 += f"├ @{admin.username}\n"    
+      else:    
+        admin = adminList.pop(0)
+        if admin.username == None:
+          text2 += f"└ {admin.mention}\n\n"
+        else:
+          text2 += f"└ @{admin.username}\n\n"
+      text2 += f"✅ | **Total number of admins**: {lenAdminList}\n❌ | Bots and hidden admins were rejected."  
+      await teletips.send_message(message.chat.id, text2)           
+  except FloodWait as e:
+    await asyncio.sleep(e.value)       
+
+@teletips.on_message(filters.command("bots", "bots@SSmusicledy_bot"))
+async def bots(client, message):  
+  try:    
+    botList = []
+    async for bot in teletips.get_chat_members(message.chat.id, filter=enums.ChatMembersFilter.BOTS):
+      botList.append(bot.user)
+    lenBotList = len(botList) 
+    text3  = f"**BOT LIST - {message.chat.title}**\n\n🤖 Bots\n"
+    while len(botList) > 1:
+      bot = botList.pop(0)
+      text3 += f"├ @{bot.username}\n"    
+    else:    
+      bot = botList.pop(0)
+      text3 += f"└ @{bot.username}\n\n"
+      text3 += f"✅ | **Total number of bots**: {lenBotList}"  
+      await teletips.send_message(message.chat.id, text3)
+  except FloodWait as e:
+    await asyncio.sleep(e.value)
