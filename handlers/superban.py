4ed1d39a8effb80689506d82e244632733190d36
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-23 10:54:56 +0400

    Update superban.py

--- handlers/superban.py
+++ handlers/superban.py
@@ -1,9 +1,35 @@
 from date import datetime
-from helpers.decorators import authorized_users_only, sudo_users_only
+
+from helpers.decorators import authorized_users_only, sudo_users_only, humanbytes
 from pyrogram import Client as app, filters
 from pyrogram.types import (
 	Message
 	)
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ("həft", 60 * 60 * 24 * 7),
+    ("gun", 60 * 60 * 24),
+    ("saat", 60 * 60),
+    ("dəqiq", 60),
+    ("saniy", 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "ə"))
+    return ', '.join(parts)
+
+
+
 
 @app.on_message(filters.command("ban") & filters.group)
 @sudo_users_only
