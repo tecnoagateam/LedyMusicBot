be65f787ae42806bad3286e5b909de747023b0f1
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-23 18:16:41 +0400

    Update unban.py

--- handlers/unban.py
+++ handlers/unban.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
-from plugins.helper_functions.admin_check import admin_check
-from plugins.helper_functions.extract_user import extract_user
+from helpers.admin_check import admin_check
+from helpers.extract_user import extract_user
 
 
 @Client.on_message(filters.command(["unban", "unmute"]))
