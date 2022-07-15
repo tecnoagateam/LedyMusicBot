5c60be04c97fcb8f2834849394052e1f7451e049
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-07-15 18:07:42 +0400

    Update uptime.py

--- handlers/uptime.py
+++ handlers/uptime.py
@@ -9,11 +9,11 @@ from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 START_TIME = datetime.utcnow()
 START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
 TIME_DURATION_UNITS = (
-    ("həft", 60 * 60 * 24 * 7),
+    ("həftə", 60 * 60 * 24 * 7),
     ("gun", 60 * 60 * 24),
     ("saat", 60 * 60),
-    ("dəqiq", 60),
-    ("saniy", 1)
+    ("dəqiqə", 60),
+    ("saniyə", 1)
 )
 
 async def _human_time_duration(seconds):
@@ -24,7 +24,7 @@ async def _human_time_duration(seconds):
         amount, seconds = divmod(int(seconds), div)
         if amount > 0:
             parts.append('{} {}{}'
-                         .format(amount, unit, "" if amount == 1 else "ə"))
+                         .format(amount, unit, "" if amount == 1 else ""))
     return ', '.join(parts)
 
 
